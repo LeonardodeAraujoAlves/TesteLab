45da1bbce3572f8f77060cc24217cac72e66e614
graduacao <graduacao@NITLAB711.acad.local>
2023-05-12 09:47:23 -0300

    Commit do laboratório 7

---
+++ TesteLab/src/testelab/TesteLab.java
@@ -0,0 +1,17 @@
+package testelab;
+
+/**
+ *
+ * @author graduacao
+ */
+public class TesteLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Commit do lab7");
+    }
+    
+}
